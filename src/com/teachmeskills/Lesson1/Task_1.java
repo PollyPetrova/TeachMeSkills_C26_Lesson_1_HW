ffb35f5d60e7c2f628c9d12cb7be0e5e75b02a6f
PollyPetrova <polina.petrova.030325@gmail.com>
2023-11-05 13:35:53 +0300

    Lesson_1
    First commit

---
+++ src/com/teachmeskills/Lesson1/Task_1.java
@@ -0,0 +1,9 @@
+package com.teachmeskills.Lesson1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, team!");
+
+    }
+}
---
+++ src/com/teachmeskills/Lesson1/Task_2.java
@@ -0,0 +1,10 @@
+package com.teachmeskills.Lesson1;
+import java.util.Scanner;
+
+public class Task_2  {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String user_name = scanner.nextLine();
+        System.out.println("Hello, "+user_name+ "!");
+    }
+}
